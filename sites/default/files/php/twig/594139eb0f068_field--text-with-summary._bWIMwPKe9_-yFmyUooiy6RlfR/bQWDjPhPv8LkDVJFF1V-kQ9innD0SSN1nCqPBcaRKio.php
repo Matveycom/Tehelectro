81d6fe747e82f46ade09c021989eb984a54c7be5
Tolik <Matveycom@rambler.ru>
2017-07-18 13:27:54 +0300

    logo

---
+++ sites/default/files/php/twig/594139eb0f068_field--text-with-summary._bWIMwPKe9_-yFmyUooiy6RlfR/bQWDjPhPv8LkDVJFF1V-kQ9innD0SSN1nCqPBcaRKio.php
@@ -0,0 +1,70 @@
+<?php
+
+/* core/themes/classy/templates/field/field--text-with-summary.html.twig */
+class __TwigTemplate_3a2d7bba91007f1c82e41e963ad749ec8f05822e48356aed9d6f7266c6be98f5 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("field--text.html.twig", "core/themes/classy/templates/field/field--text-with-summary.html.twig", 1);
+        $this->blocks = array(
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "field--text.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/field/field--text-with-summary.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  11 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{% extends \"field--text.html.twig\" %}
+";
+    }
+}
